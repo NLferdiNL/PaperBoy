3beef01b9c7c3f894ba6470faca832f2082941c6
NLferdiNL <nlferdinl@gmail.com>
2016-02-13 17:47:52 +0100

    Project push( with minor changes already )

---
+++ Assets/Scripts/Managers/DiscoSetting.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class DiscoSetting : MonoBehaviour
+{
+	private static bool spawned = false;
+
+	public AudioClip DiscoClip;
+
+	public bool IsDisco = false; 
+
+	void Awake()
+	{
+		if(DiscoClip == null)
+			DiscoClip = (AudioClip)Resources.Load("Disco");
+
+		if(spawned == false)
+		{
+			spawned = true;
+			
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			DestroyImmediate(gameObject);
+		}
+	}
+
+	void Start()
+	{
+		if(SceneManager.GetActiveScene().name == "PlayScene")
+		{
+			Global.Instance.IsDisco = IsDisco;
+			Camera.main.gameObject.GetComponent<AudioSource>().clip = DiscoClip;
+		}
+	}
+}
---
+++ Assets/Scripts/World/Pickup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PickupType
+{
+	None = -1,
+	Bike = 0,
+	SteelPaper = 1,
+	GoldenPaper = 2,
+	ColaBottle = 3
+}
+
+public class Pickup : BasicObstacleBehaviour
+{
+	public PickupType Type;
+
+	protected override void Move ()
+	{
+		base.Move ();
+	}
+
+	void OnTriggerEnter2D(Collider2D Coll)
+	{
+		if(Coll.tag == "Player")
+		{
+			Coll.GetComponent<PlayerController>().SetPickup(Type);
+
+			Destroy(gameObject);
+		}
+	}
+}
